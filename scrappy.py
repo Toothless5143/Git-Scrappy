923b3604d022a5dfbeb9d2eb682e9facc435e55c
Safwan Luban <81353028+Toothless5143@users.noreply.github.com>
2023-07-05 19:41:00 +0600

    Update scrappy.py

--- scrappy.py
+++ scrappy.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 
 # Set the path to your chromedriver
-cdp = "./chromedriver" # Change it according to your own needs
+cdp = "#" # Change it according to your own need
 driver = webdriver.Chrome(executable_path=cdp)
 
 # Prompt the user to enter the username
