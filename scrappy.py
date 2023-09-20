60dd00101432ea12231a6aded5960ca1b22787cb
Safwan Luban <81353028+Toothless5143@users.noreply.github.com>
2023-09-20 09:15:22 +0600

    Update scrappy.py

--- scrappy.py
+++ scrappy.py
@@ -1,74 +1,51 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-import time
+import requests
+import re
 
-# Set the path to your chromedriver
-cdp = "/home/toothless/Downloads/chromedriver"  # Change it according to your own need
-
-# Set Chrome options
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument(f"executable_path={cdp}")
-
-# Initialize Chrome driver
-driver = webdriver.Chrome(options=chrome_options)
-
-# Prompt the user to enter the GitHub username
-username = input("Enter the GitHub username: ")
-
-# Visit the user's GitHub profile
-url = f"https://github.com/{username}"
-driver.get(url)
-
-# Wait for the page to load (optional)
-time.sleep(2)
-
-# Find all repositories
-repos = driver.find_elements(By.CLASS_NAME, "repo")
-
-# Wait for the elements to load (optional)
-time.sleep(2)
-
-# Loop through repositories
-for repo in repos:
-    repo_name = repo.text
-    print(f"Scanning repository: {repo_name}")
+def scan_repositories(username):
+    # Make a request to the GitHub API to fetch the user's repositories
+    url = f"https://api.github.com/users/{username}/repos"
+    response = requests.get(url)
     
-    try:
-        # Open the repository
-        repo_link = repo.find_element(By.TAG_NAME, "a").get_attribute("href")
-        driver.get(repo_link)
-        
-        # Wait for the page to load (optional)
-        time.sleep(2)
-        
-        # Find all files in the repository
-        files = driver.find_elements(By.CLASS_NAME, "js-navigation-open")
-        
-        # Loop through files
-        for file in files:
-            file_name = file.text
-            file_link = file.get_attribute("href")
-            driver.get(file_link)
-            
-            # Wait for the page to load (optional)
-            time.sleep(2)
-            
-            # Get the file content
-            file_content = driver.find_element(By.CLASS_NAME, "highlight").text
-            
-            # Check if "funny" is present in the file content
-            if "password" in file_content:
-                print(f"Found 'password' in file: {file_name}")
-                lines = file_content.split("\n")
-                
-                # Print lines containing "funny"
-                for line in lines:
-                    if "funny" in line:
-                        print(line)
-    
-    except NoSuchElementException:
-        print("No repositories found.")
-    
-# Quit the driver
-driver.quit()
+    if response.status_code != 200:
+        print(f"Failed to fetch repositories for user {username}.")
+        return
+
+    repositories = response.json()
+
+    # Iterate over each repository
+    for repo in repositories:
+        try:
+            repo_name = repo["name"]
+            print(f"Scanning repository: {repo_name}")
+
+            # Make a request to fetch the contents of the repository
+            contents_url = f"https://api.github.com/repos/{username}/{repo_name}/contents"
+            contents_response = requests.get(contents_url)
+            contents = contents_response.json()
+
+            # Iterate over each file in the repository
+            for item in contents:
+                if item["type"] == "file":
+                    file_url = item["download_url"]
+
+                    # Make a request to fetch the file contents
+                    file_response = requests.get(file_url)
+                    file_contents = file_response.text
+
+                    # Check if the file contains any usernames or passwords using regex pattern
+                    pattern = r"(?i)\b(?:user(?:name)?|pass(?:word)?)\b:\s*(\S+)"
+                    matches = re.findall(pattern, file_contents)
+
+                    # Print the matches
+                    for match in matches:
+                        print(f"In file: {item['name']}")
+                        print(f"Match: {match}")
+                        print("-" * 50)
+
+        except KeyError:
+            print("Error accessing repository information.")
+            continue
+
+# Ask for GitHub username input
+username = input("Enter a GitHub username: ")
+scan_repositories(username)
